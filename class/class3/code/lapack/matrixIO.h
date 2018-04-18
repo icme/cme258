83c1ffae4d75c985a1f1b9eb7240fb2012377248
Brad Nelson <bjnelson@stanford.edu>
2018-04-18 10:31:14 -0700

    add comment to matrixIO header

--- class/class3/code/lapack/matrixIO.h
+++ class/class3/code/lapack/matrixIO.h
@@ -1,5 +1,6 @@
 // header-only file for print_matrix function.
 
+// print (m x n) matrix A assuming column-major ordering.
 void print_matrix(double *A, int m, int n) {
 	for (int i = 0; i < m; i++) {
 		for (int j = 0; j < n; j++) {
